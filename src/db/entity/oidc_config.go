f247be39cdd5d14c434654c7c1d2e6b1808fa9c7
Niklas Zagler <niklas.zagler@pm.me>
2026-05-08 21:29:29 +0200

    ADD OpenID Connect (OIDC) configuration and management features

---
+++ src/db/entity/oidc_config.go
@@ -0,0 +1,10 @@
+package entity
+
+type OIDCConfig struct {
+	ID           int    `gorm:"primary_key;autoIncrement"`
+	IssuerURL    string `gorm:"not null"`
+	ClientID     string `gorm:"not null"`
+	ClientSecret string `gorm:"not null"`
+	Scopes       string `gorm:"not null;default:openid profile email"`
+	Enabled      bool   `gorm:"not null;default:true"`
+}
